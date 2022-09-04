146f74c7aaadcb929c9f68eb9aa91d165a76a4c3
Mihail Kostov <81187120+MK-Kostov@users.noreply.github.com>
2022-09-04 15:19:53 +0300

    Add React.lazy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, lazy } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import uniqid from "uniqid";
 
@@ -7,11 +7,11 @@ import * as gameService from "./components/services/gameService";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
 import CreateGame from "./components/CreateGame/CreateGame";
 import Catalog from "./components/Catalog/Catalog";
 import GameDetails from "./components/GameDetails/GameDetails";
 import "./App.css";
+const Register = lazy(() => import("./components/Register/Register"));
 
 function App() {
   const [games, setGames] = useState([]);
